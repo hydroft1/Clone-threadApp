e1c6ff0a24806c1f44b56930205cb3b5c45c6d1f
hydroft1 <marquetalexandre@proton.me>
2024-02-16 23:30:03 +0100

    create register view

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,159 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Pressable,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const RegisterScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View style={{ marginTop: 50 }}>
+        <Image
+          style={{ width: 150, height: 100, reziseMode: "contain" }}
+          source={{
+            uri: "https://freelogopng.com/images/all_img/1688663386threads-logo-transparent.png",
+          }}
+        />
+      </View>
 
-export default RegisterScreen
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold", marginTop: 25 }}>
+            Login to your account
+          </Text>
+        </View>
 
-const styles = StyleSheet.create({})
+        <View style={{ marginTop: 40 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              borderColor: "#D0D0D0",
+              borderWidth: 1,
+              paddingVertical: 5,
+              borderRadius: 5,
+            }}
+          >
+            <MaterialIcons
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              placeholderTextColor={"gray"}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 16 : 16,
+              }}
+              placeholder="enter your email"
+            />
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                borderColor: "#D0D0D0",
+                borderWidth: 1,
+                paddingVertical: 5,
+                borderRadius: 5,
+              }}
+            >
+              <AntDesign
+                style={{ marginLeft: 8 }}
+                name="lock"
+                size={24}
+                color="gray"
+              />
+              <TextInput
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                placeholderTextColor={"gray"}
+                style={{
+                  color: "gray",
+                  marginVertical: 10,
+                  width: 300,
+                  fontSize: password ? 16 : 16,
+                }}
+                placeholder="enter your Password"
+              />
+            </View>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginTop: 12,
+            }}
+          >
+            <Text>Keep me logged in</Text>
+            <Text style={{ fontWeight: "500", color: "#007FFF" }}>
+              Forgot Password
+            </Text>
+          </View>
+        </View>
+
+        <View style={{ marginTop: 45 }} />
+
+        <Pressable
+          style={{
+            width: 200,
+            backgroundColor: "black",
+            padding: 15,
+            marginTop: 40,
+            marginLeft: "auto",
+            marginRight: "auto",
+            borderRadius: 6,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 16,
+              color: "white",
+            }}
+          >
+            Login
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() => navigation.navigate("Register")}
+          style={{ marginTop: 10 }}
+        >
+          <Text style={{ textAlign: "center", fontSize: 16 }}>
+            Don't have an account? Sign up
+          </Text>
+        </Pressable>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({});
